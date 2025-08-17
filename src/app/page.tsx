caea5eaa84a377b012f2edca2925ab95e038d81b
CARLOS LIRA <carboo12@gmail.com>
2025-08-17 03:38:10 +0000

    Por lo momento esta versión que vamos a soltar en una versión privada de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,11 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Eye, EyeOff } from "lucide-react"
-import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "@/lib/firebase";
 import { useToast } from "@/hooks/use-toast";
-import Image from "next/image";
 import { Logo } from "@/components/logo";
 
 
@@ -75,15 +73,7 @@ export default function LoginPage() {
               />
             </div>
             <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Contraseña</Label>
-                <Link
-                  href="#"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  ¿Olvidaste tu contraseña?
-                </Link>
-              </div>
+              <Label htmlFor="password">Contraseña</Label>
               <div className="relative">
                 <Input id="password" name="password" type={showPassword ? "text" : "password"} required />
                 <Button
@@ -102,12 +92,6 @@ export default function LoginPage() {
               Iniciar Sesión
             </Button>
           </form>
-          <div className="mt-4 text-center text-sm">
-            ¿No tienes una cuenta?{" "}
-            <Link href="#" className="underline">
-              Regístrate
-            </Link>
-          </div>
         </CardContent>
       </Card>
     </div>
