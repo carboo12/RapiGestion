3595b9e18f73b003745d6142122befe76d24491a
CARLOS LIRA <carboo12@gmail.com>
2025-08-17 00:31:22 +0000

    centra la palabra cliente

--- src/app/(app)/clients/page.tsx
+++ src/app/(app)/clients/page.tsx
@@ -34,7 +34,7 @@ const clients = [
 export default function ClientsPage() {
   return (
     <div className="space-y-4">
-      <div>
+      <div className="text-center">
         <h2 className="text-3xl font-bold tracking-tight">Clientes</h2>
       </div>
       <div>
